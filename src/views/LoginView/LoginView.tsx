00cb00146a83df8e690f1acdc4fdee74c9218d39
vitoriapinheiro <vps3@cin.ufpe.br>
2021-05-02 14:27:00 -0300

    hotfix(star): aligned the star

--- src/views/LoginView/LoginView.tsx
+++ src/views/LoginView/LoginView.tsx
@@ -6,6 +6,7 @@ import firebase from 'firebase/app'
 
 import {
   Box,
+  Container,
 } from './styles';
 
 function LoginView() {
@@ -42,13 +43,15 @@ function LoginView() {
   },[])
   return (
     <>
-      <Card 
-        likes={2} 
-        dislikes={2} 
-        img={'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQcYbM174xWDaTnMb40TN_IbfKPqD4bY7TN4Q&usqp=CAU'}
-        logo={'https://marcas-logos.net/wp-content/uploads/2019/11/Simbolo-Star-Wars.jpg'}
-        topPost={true}
-        />
+      <Container>
+        <Card 
+          likes={2} 
+          dislikes={2} 
+          img={'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQcYbM174xWDaTnMb40TN_IbfKPqD4bY7TN4Q&usqp=CAU'}
+          logo={'https://marcas-logos.net/wp-content/uploads/2019/11/Simbolo-Star-Wars.jpg'}
+          topPost={true}
+          />
+      </Container>
     </>
   );
 }
--- src/views/LoginView/styles.ts
+++ src/views/LoginView/styles.ts
@@ -1,6 +1,10 @@
 import styled from 'styled-components';
 import TextField from '@material-ui/core/TextField';
 
-export const Box = styled.div`
+export const Box = styled.div``;
 
+export const Container = styled.div`
+  margin-top: 40px;
 `;
+
+
