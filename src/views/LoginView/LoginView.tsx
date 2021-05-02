f13aaf00d9c41138523c93682337f780b72b382e
Sofiamdl <sml#cin.ufpe.br>
2021-05-02 09:51:28 -0300

    Feature(firebase-test): working

--- src/views/LoginView/LoginView.tsx
+++ src/views/LoginView/LoginView.tsx
@@ -1,11 +1,45 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Card } from '../../components';
+import {todosRef} from '../../backend/firebase';
+import "firebase/firestore"
+import firebase from 'firebase/app'
 
 import {
   Box,
 } from './styles';
 
 function LoginView() {
+  var db = firebase.firestore();
+  db.collection("Hacka").get().then((querySnapshot) => {
+    console.log(querySnapshot)
+    querySnapshot.forEach((doc) => {
+        console.log(doc.data());
+    });
+  });
+  useEffect(() => {
+    todosRef.once('value', (snapshot) => {
+      let items = snapshot.val();
+      console.log(snapshot)
+      console.log(items)
+      // let newState = [];
+      // for (let item in items) {
+      //   newState.push({
+      //     id: item,
+      //     task: items[item].task,
+      //     done: items[item].done
+      //   });
+      // }
+    });
+    todosRef.child("Card").get().then((snapshot) => {
+      if (snapshot.exists()) {
+        console.log(snapshot.val());
+      } else {
+        console.log("No data available");
+      }
+    }).catch((error) => {
+      console.error(error);
+    });
+  },[])
   return (
     <>
       <Card 
